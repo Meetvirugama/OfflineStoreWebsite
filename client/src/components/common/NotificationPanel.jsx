cf31a0cd98a3e2f1a8964579f5937b2372aec012
Meet Virugama <meetvirugama@Meets-MacBook-Air.local>
2026-04-08 11:46:39 +0530

    fix: notification deletion and google login redirect improvements

--- client/src/components/common/NotificationPanel.jsx
+++ client/src/components/common/NotificationPanel.jsx
@@ -8,7 +8,8 @@ export default function NotificationPanel() {
     fetchNotifications,
     openNotification,
     clickNotification,
-    markAllAsRead
+    markAllAsRead,
+    deleteNotification
   } = useNotificationStore();
 
   useEffect(() => {
